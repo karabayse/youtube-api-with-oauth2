f980a291cdc025e2fd4879a5fea6e1e5e1161d4d
Kara Bayse <karabayse@gmail.com>
2018-07-19 12:36:11 -0500

    server up

--- server.js
+++ server.js
@@ -5,9 +5,14 @@ var path = require('path');
 var bodyParser = require('body-parser');
 
 // Uses
-// app.use(express.static('public'));
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.get('/*', function(req, res) {
+  console.log('base url hit');
+  res.sendFile(path.resolve('public/views/index.html'));
+}); // 
 
 // Globals
 var port = process.env.PORT || 7070;
