3d444216502aedf4c54c4a168744df72f3466ebe
Kara Bayse <karabayse@gmail.com>
2018-07-30 14:17:14 -0500

    numbers with commas wrap

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -92,19 +92,40 @@ function getChannel(channel) {
       <ul class="collection">
         <li class="collection-item">Title: ${channel.snippet.title}</li>
         <li class="collection-item">ID: ${channel.id}</li>
-        <li class="collection-item">Subscribers: ${channel.statistics.subscriberCount}</li>
-        <li class="collection-item">views: ${channel.statistics.viewCount}</li>
-        <li class="collection-item">Videos: ${channel.statistics.videoCount}</li>
+        <li class="collection-item">Subscribers: ${numberWithCommas(channel.statistics.subscriberCount)}</li>
+        <li class="collection-item">views: ${numberWithCommas(channel.statistics.viewCount)}</li>
+        <li class="collection-item">Videos: ${numberWithCommas(channel.statistics.videoCount)}</li>
       </ul>
       <p>${channel.snippet.description}</p>
       <hr>
       <a class="btn grey darken-2" target="_blank" href="https://youtube.com/${channel.snippet.customUrl}">Visit Channel</a>
     `;
     showChannelData(output);
+
+    const playlistId = channel.contentDetails.relatedPlaylists.uploads;
+    requestVideoPlaylist(playlistId);
   })
   .catch(err => alert('No Channel By That Name'));
 }
 
+// Add commas to number
 function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d)/g, ","));
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
+
+function requestVideoPlaylist(playlistId) {
+  const requestOptions = {
+    playlistId: playlistId,
+    part: 'snippet',
+    maxResults: 10
+  }
+  const request = gapi.client.youtube.playlistItems.list(requestOptions);
+
+  request.execute(response => {
+    console.log(response);
+    const playListItems = response.result.items;
+    if(playListItems) {
+      
+    }
+  });
+}
